aadc04259bc3f0d073595cdb53535178442f653c
anika-chhoa <chhoa.anika07@gmail.com>
2026-06-24 07:52:10 +0600

    Update user role using PATCH API

--- index.js
+++ index.js
@@ -37,10 +37,23 @@ async function run() {
     const favoriteCollection = database.collection("favorites");
 
     //all users
-    app.get("/api/user", async (req, res) => {
+    app.get("/api/users", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
+
+    app.patch("/api/users/:id",async(req,res)=>{
+      const id = req.params.id;
+      const { role } = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: { role: role },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
 
     //properties
     app.get("/api/properties", async (req, res) => {
